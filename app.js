84f00941f4f2a4d8d596c3038ca2f6453455d80b
Rennie Bevineau <rsbevineau@gmail.com>
2020-06-28 10:54:30 -0400

    complete project structure

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+require('dotenv').config()
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors')
+const helmet = require('helmet')
+const { NODE_ENV } = require('./config')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,7 +17,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use(logger('dev'));
+app.use(logger((NODE_ENV === 'production') ? 'tiny' : 'common'))
+app.use(cors())
+app.use(helmet())
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
---
+++ test/setup.js
@@ -0,0 +1,6 @@
+process.env.TZ = 'UTC'
+require('dotenv').config()
+const { expect } = require('chai')
+const supertest = require('supertest')
+global.expect = expect
+global.supertest = supertest
