3e2a4616e71f648f7a96486b339dd3f99dceb800
Rennie Bevineau <rsbevineau@gmail.com>
2020-07-01 21:12:01 -0400

    add a post route for a new question

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var questionService = require('../services/question-service')
+const jsonBodyParser = express.json()
+
+const serializeQuestion = question => ({
+  id: question.id,
+  answers: question.answers,
+  questiontext: question.questiontext,
+  responsetext: question.responsetext,
+  correcttext: question.correcttext,
+  audio: question.audio
+})
+
 
 /* GET users listing. */
 router.get('/questions/:id', function(req, res, next) {
@@ -17,8 +28,32 @@ router.get('/questions/:id', function(req, res, next) {
     }
     
     res.json(data);
+  
     
   })
 });
+
+router.route('/questions').post(jsonBodyParser, function(req,res,next){
+  const {answers, questiontext, responsetext, correcttext, audio} = req.body
+  const newQuestion = {answers, questiontext, responsetext, correcttext, audio}
+
+  console.log(answers)
+  console.log(questiontext)
+
+  for (const [key, value] of Object.entries(jsonBodyParser))
+      if (value == null)
+        return res.status(400).json({
+          error: { message: `Missing '${key}' in request body` }
+  })
+
+
+  questionService.insertQuestion(req.app.get('db'), newQuestion)
+  .then(question => {
+    res
+      .status(201)
+      .json(serializeQuestion(question))
+  })
+  .catch(next)
+})
 
 module.exports = router;
--- services/question-service.js
+++ services/question-service.js
@@ -2,6 +2,11 @@ const questionService = {
 getById(knex, id) {
     return knex.from('questions').select('*').where('id', id)
   },
+insertQuestion(db, newQuestion){
+  return db
+    .insert(newQuestion)
+    .into('questions')
+}
 }
 
 module.exports = questionService
