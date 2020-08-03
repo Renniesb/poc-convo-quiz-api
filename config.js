272b23566f86c1939d54cfc4e545a336bd79eadd
Rennie Bevineau <rsbevineau@gmail.com>
2020-08-03 12:08:19 -0400

    change db url to database url

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/quiz',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/quiz',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/quiz-test'
 }
