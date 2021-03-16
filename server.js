a552f1d663776d7987d013d989150517c863cfaf
Rennie Bevineau <rsbevineau@gmail.com>
2021-03-16 00:10:20 -0400

    add reject unauthorized to server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const pg = require('pg');
-pg.defaults.ssl = process.env.NODE_ENV === "production";
+pg.defaults.ssl = process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false;
 const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
@@ -7,7 +7,7 @@ const { PORT, DATABASE_URL } = require('./config')
 //establish db connection and listen on the port specified in config
 const db = knex({
     client: 'pg',
-    connection: DATABASE_URL,
+    connection: DATABASE_URL
 })
 
 app.set('db', db)
